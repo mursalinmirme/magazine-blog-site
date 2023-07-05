347fbe5bb25a7e7b4538d2b492e3063f2714b68a
mursalinmirme <mursalinmir02@gmail.com>
2023-07-06 03:04:11 +0600

    first time upload blogger site

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$('.owlCarouselOne').owlCarousel({
+    loop:true,
+    margin:17,
+    padding:0,
+    items:1,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:1,
+            loop:true
+        }
+    }
+})
+$('.owlCarouselTwo').owlCarousel({
+    loop:true,
+    margin:17,
+    padding:0,
+    items:4,
+    center:true,
+    responsiveClass:true,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:3,
+            loop:true
+        }
+    }
+})
